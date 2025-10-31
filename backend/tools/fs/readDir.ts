8ec049bebecb45da0b2d735930281bf637a8f6f5
R-Rajaneesh <rajaneeshr2006@gmail.com>
2025-10-31 23:30:26 +0530

    ➕ Basic Agent Tools for Fill Operations.

---
+++ backend/tools/fs/readDir.ts
@@ -0,0 +1,14 @@
+import fs from "fs-extra";
+import type { Tool } from "ollama";
+const readDir = async (dirPath: string) => {
+	const dir_contents = await fs.readdirSync(dirPath);
+	return dir_contents;
+};
+export default {
+	type: "function",
+	function: {
+		name: "readDir",
+		description: "Read all files inside a directory.",
+		parameters: { dirPath: { type: String, description: "Directory Path to search." } },
+	},
+} as Tool;
---
+++ backend/tools/fs/readFile.ts
@@ -0,0 +1,14 @@
+import fs from "fs-extra";
+import type { Tool } from "ollama";
+const readFile = async (filePath: string) => {
+	const file_contents = await fs.readFileSync(filePath);
+	return file_contents;
+};
+export default {
+	type: "function",
+	function: {
+		name: "readFile",
+		description: "Read contents of file.",
+		parameters: { filePath: { type: String, description: "File Path to search and manipulate." } },
+	},
+} as Tool;
