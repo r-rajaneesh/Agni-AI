7c255c7ba07fcc32e0b0a9d01f4118164aee3be8
R-Rajaneesh <rajaneeshr2006@gmail.com>
2025-10-31 23:34:16 +0530

    🔁 Fix and standaerdize all tool call export.

--- backend/tools/fs/readDir.ts
+++ backend/tools/fs/readDir.ts
@@ -1,6 +1,6 @@
 import fs from "fs-extra";
 import type { Tool } from "ollama";
-const readDir = async (dirPath: string) => {
+const readDir = async (dirPath: string ="./") => {
 	const dir_contents = await fs.readdirSync(dirPath);
 	return dir_contents;
 };
@@ -9,6 +9,9 @@ export default {
 	function: {
 		name: "readDir",
 		description: "Read all files inside a directory.",
-		parameters: { dirPath: { type: String, description: "Directory Path to search." } },
+		parameters: {
+			type: "object",
+			dirPath: { type: String, description: "Directory Path to search." },
+		},
 	},
 } as Tool;
