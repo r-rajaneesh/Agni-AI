94af36b0150ade9fcfd4fb6d4e2b0bff18327fe4
R-Rajaneesh <rajaneeshr2006@gmail.com>
2025-11-01 00:50:35 +0530

    🔁 Fixed and modified features and bugs in tool calling.

--- backend/tools/fs/readFile.ts
+++ backend/tools/fs/readFile.ts
@@ -1,8 +1,12 @@
 import fs from "fs-extra";
 import type { Tool } from "ollama";
-const readFile = async (filePath: string) => {
-	const file_contents = await fs.readFileSync(filePath);
-	return file_contents;
+const readFile = async (params: { filePath: string }) => {
+	try {
+		const file_contents = await fs.readFileSync(params.filePath).toString();
+		return file_contents;
+	} catch (error) {
+		return `Some error occoured while reading ${params.filePath}, maybe a directory.`;
+	}
 };
 export default {
 	type: "function",
@@ -11,7 +15,9 @@ export default {
 		description: "Read contents of file.",
 		parameters: {
 			type: "object",
-			filePath: { type: String, description: "File Path to search and manipulate." },
+			properties: {
+				filePath: { type: "string", description: "File Path to search and manipulate." },
+			},
 		},
 	},
 	execute: readFile,
